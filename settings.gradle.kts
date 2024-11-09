d1bd5bbfff15b67f4e7e2b2b0642bc54dc244798
Rasmus Lindholm <124736358+lira21ol@users.noreply.github.com>
2024-11-09 16:41:45 +0100

    Start av projektet tillagda mappar

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TicTacToe Android Studio Rasmus.L"
+include(":app")
+ 
